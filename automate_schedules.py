6babb4cdf89f601df251c185bae0aaf610e2761a
Amina <42360103+aventon1@users.noreply.github.com>
2023-02-05 02:36:06 -0500

    Initial Commit

---
+++ automate_schedules.py
@@ -0,0 +1,9 @@
+from openpyxl import Workbook
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+
+    # Create excel workbook
+    wb = Workbook()
+    ws = wb.active
