c2d3b7facc0634d923901aea54070ee496a920c5
Amina <42360103+aventon1@users.noreply.github.com>
2023-02-05 03:12:51 -0500

    Read csv data to a dictionary and wrote to xlsx file

--- automate_schedules.py
+++ automate_schedules.py
@@ -1,9 +1,28 @@
+import csv
 from openpyxl import Workbook
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
 
     # Create excel workbook
     wb = Workbook()
     ws = wb.active
+
+    # Get csv file
+    # TODO: Fix to get this user to input
+    csv_filename = "example.csv"
+
+    # Write csv data to excel files
+    # TODO: Check encoding
+    with open(csv_filename, encoding='utf-8-sig') as csv_file:
+
+        # Add csv data to a dictionary
+        csv_reader = csv.DictReader(csv_file)
+
+        for csv_row in csv_reader:
+
+            # TODO: Only insert specific columns
+            # TODO: Get header data
+            ws.append(csv_row)
+
+    wb.save(filename='Schedule.xlsx')
