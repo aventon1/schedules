1c4daeb13ad3a6601752897cd4fcfae6cc2d1230
Amina <42360103+aventon1@users.noreply.github.com>
2023-02-06 01:50:43 -0500

    Created new function to write csv to excel sheet

--- automate_schedules.py
+++ automate_schedules.py
@@ -3,9 +3,45 @@ from openpyxl import Workbook
 import re
 
 
+def write_csv_to_xlsx(csv_filename, ws):
+
+
+    # Write csv data to excel files
+    # TODO: Check encoding
+    with open(csv_filename, encoding='utf-8-sig') as csv_file:
+
+        # Add csv data to a dictionary
+        csv_reader = csv.DictReader(csv_file)
+
+        # Variables to get specific header data
+        provider_name = ''
+        date_range = ''
+        practice_name = ''
+        iteration = 0
+        for csv_row in csv_reader:
+
+            # Capture header data
+            if iteration == 0:
+                provider_name = get_provider(csv_row['Textbox9'])
+                date_range = csv_row['textbox29']
+                practice_name = csv_row['PracticeName']
+                iteration += 1
+
+            # Insert only these values into rows
+            patient_appointment = [csv_row['AppointmentTime'],
+                                   csv_row['Patient'],
+                                   csv_row['Comments'],
+                                   csv_row['PatientEmailAddress'],
+                                   csv_row['AppointmentTypeName'],
+                                   csv_row['Carrier'],
+                                   csv_row['Provider']]
+            ws.append(patient_appointment)
+
+    return ws, provider_name, date_range, practice_name
+
+
 def get_provider(provider_string):
 
-    provider_name = ""
 
     provider_match = re.search(r'^P.+(,\s)', provider_string)
 
@@ -27,36 +63,6 @@ if __name__ == '__main__':
     # TODO: Fix to get this user to input
     csv_filename = "example.csv"
 
-    # Write csv data to excel files
-    # TODO: Check encoding
-    with open(csv_filename, encoding='utf-8-sig') as csv_file:
-
-        # Add csv data to a dictionary
-        csv_reader = csv.DictReader(csv_file)
-
-        # Variables to get specific header data
-        provider_name = ''
-        date_range = ''
-        practice_name = ''
-        iteration = 0
-
-        for csv_row in csv_reader:
-
-            # Capture header data
-            if iteration == 0:
-                provider_name = get_provider(csv_row['Textbox9'])
-                date_range = csv_row['textbox29']
-                practice_name = csv_row['PracticeName']
-                iteration += 1
-
-            # Insert only these values into rows
-            patient_appointment = [csv_row['AppointmentTime'],
-                                   csv_row['Patient'],
-                                   csv_row['Comments'],
-                                   csv_row['PatientEmailAddress'],
-                                   csv_row['AppointmentTypeName'],
-                                   csv_row['Carrier'],
-                                   csv_row['Provider']]
-            ws.append(patient_appointment)
+    ws, provider_name, date_range, practice_name = write_csv_to_xlsx(csv_filename, ws)
 
     wb.save(filename='Schedule.xlsx')
